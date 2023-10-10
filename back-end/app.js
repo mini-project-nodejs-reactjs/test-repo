dcd37b736f90d3dbcfab96f52a2457c3a9eb6eee
Maulanaaryo <Maulanaaryo151@gmail.com>
2023-10-11 00:02:47 +0700

    add cors

--- back-end/app.js
+++ back-end/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const routes = require("./routes");
+const cors = require("cors");
 
 require("dotenv").config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
