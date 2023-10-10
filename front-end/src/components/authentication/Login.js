8d8c143983c79d3c33c9e8826813ebe6e6164695
Maulanaaryo <Maulanaaryo151@gmail.com>
2023-10-10 15:16:00 +0700

    add auth

---
+++ front-end/src/components/authentication/Login.js
@@ -0,0 +1,61 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 mx-auto text-center">
+          <form>
+            <img
+              src={
+                "https://logos-world.net/wp-content/uploads/2021/03/Trello-Logo.png"
+              }
+              alt=""
+              className="logo mb-4"
+            />
+            <h5 className="mb-3">Log in to continue</h5>
+            <div className="form-group mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div className="form-group mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary w-100 mb-4">
+              Continue
+            </button>
+            <h3 className="mb-3">OR</h3>
+            <button className="btn btn-light w-100 google-button">
+              <img
+                src={
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png"
+                }
+                alt="Google Logo"
+                className="google-logo"
+              />
+              <span className="google-button-text">Continue with Google</span>
+            </button>
+            <p className="mt-4">
+              Don't have an account?
+              <Link to="/register" className="create-account">
+                Create an account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
