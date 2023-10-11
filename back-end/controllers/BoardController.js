16dba9dad6683abcb2eb47a49cf4320897134e6f
Maulanaaryo <Maulanaaryo151@gmail.com>
2023-10-11 11:01:37 +0700

    menambahkan route board

---
+++ back-end/controllers/BoardController.js
@@ -0,0 +1,11 @@
+const { board } = require("../models");
+
+class BoardController {
+  static async getAllBoards(req, res) {}
+
+  static async createBoard(req, res) {}
+
+  static async getUserById(req, res) {}
+}
+
+module.exports = BoardController;
